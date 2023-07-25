9ae220f3bc57aa6452c1756297bb28e3c17f2aff
Oluwademiladeogo <bickerstethdemilade@gmail.com>
2023-07-25 16:46:52 +0100

    Little UI changes

--- server/main.js
+++ server/main.js
@@ -5,13 +5,24 @@ const winston = require("winston")
 const session = require("express-session")
 const joi = require("joi")
 var path = require('path')
+// const cookieParser = require("cookie-parser")
 //using static folder
-app.use(express.static("public"))
-app.use(express.json)
+app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 //set view engine
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
+
+//connect flash
+var flash = require('connect-flash');
+
+ 
+// app.set(()=>{
+// //   app.use(cookieParser('keyboard cat'));
+//   app.use(express-session({ cookie: { maxAge: 60000 }}));
+//   app.use(flash());
+// });
+
 
 const pages = require("./routes/pages")
 const adminpages = require("./routes/adminpages.js")
@@ -19,15 +30,16 @@ app.use("/", pages)
 app.use("/admin/pages", adminpages)
 
 //express session 
-app.set('trust proxy', 1) // trust first proxy
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: true }
-}))
+// app.set('trust proxy', 1) // trust first proxy
+// app.use(session({
+//   secret: 'keyboard cat',
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: { secure: true }
+// }))
 
-
+const log = require("./logging.js")
+log()
 
 
 
--- server/routes/pages.js
+++ server/routes/pages.js
@@ -2,9 +2,13 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", (req, res)=>{
-    res.render("index", {
+    res.render("pages/index", {
         title: "home"
     })
 })
-
+router.get("/aboutus", (req, res)=>{
+    res.render("pages/aboutus", {
+        title: 'Aboutus'
+    })
+})
 module.exports = router;
