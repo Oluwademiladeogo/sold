31b5358bd88f7790992eac941564159767040988
Oluwademiladeogo <bickerstethdemilade@gmail.com>
2023-07-26 09:29:14 +0100

    added login and cart modules then minor tweaks

--- server/routes/pages.js
+++ server/routes/pages.js
@@ -2,13 +2,15 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", (req, res)=>{
-    res.render("pages/index", {
-        title: "home"
-    })
+    res.render("pages/index")
 })
 router.get("/aboutus", (req, res)=>{
-    res.render("pages/aboutus", {
-        title: 'Aboutus'
-    })
+    res.render("pages/aboutus")
+})
+router.get("/login", (req, res)=>{
+    res.render("pages/login")
+})
+router.get("/cart", (req, res)=>{
+    res.render("pages/cart")
 })
 module.exports = router;
